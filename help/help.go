4e3bf4fe1a0200b184dd6d0c2ddb69ba41bed5af
Grover Soans <groverpsoans@gmail.com>
2024-09-20 14:42:51 -0500

    formatting changes on help page

--- help/help.go
+++ help/help.go
@@ -16,13 +16,13 @@ func Help(w http.ResponseWriter, r *http.Request) {
             <li><a href="http://` + baseUrl + `/hello-world">Hello World</a></li>
             <li><a href="http://` + baseUrl + `/hello-world-html">Hello World - HTML</a></li>
             <li><a href="http://` + baseUrl + `/hello-world-json">Hello World - JSON</a></li>
-            <li><a href="http://` + baseUrl + `/syllabus">Get Syllabus</a></li>
             <li><a href="http://` + baseUrl + `/syllabus/create">Create Syllabus</a></li>
+            <li><a href="http://` + baseUrl + `/syllabus">Read Syllabus</a></li>
             <li><a href="http://` + baseUrl + `/syllabus/update">Update Syllabus</a></li>
             <li><a href="http://` + baseUrl + `/syllabus/delete">Delete Syllabus</a></li>
         </ul>
     </body>
     </html>`
-    
+
     w.Write([]byte(htmlResponse))
 }
